fb26e55e8c54c73b5685e311b380dbba2b845a87
Eric Korolev <3254@mail.ru>
2017-07-07 15:32:45 +0500

    Change endings from \n to \r\n

--- fields_info.php
+++ fields_info.php
@@ -1,1 +1,53 @@
-<?php/** * Тестовое задание. * Используя данные аккаунты amoCRM и документацию для разработчиков написать небольшой скрипт, * который парсит выдачу метода GET accounts/current и возвращает результат: название воронки продаж,  * название статуса сделки, ID статуса сделки в формате JSON и кодом ответа сервера 200 в случае успеха.  * Код скрипта предоставить. Использование сторонних классов для api amocrm не разрешается. */ $pipelines = array();$id = array();$name = array(); if(isset($account['pipelines']) && isset($account['leads_statuses'])) {	foreach($account['pipelines'] as $field) {		if(is_array($field) && isset($field['name'])) {			array_push($pipelines, $field['name']);		} else {			die('Невозможно получить поле "Имя воронки продаж"');		} 	}		foreach($account['leads_statuses'] as $field) {		if(is_array($field) && isset($field['id'])) {			array_push($id, $field['id']);		} else {			die('Невозможно получить поле "ID статуса сделки"');		} 	}		foreach($account['leads_statuses'] as $field) {		if(is_array($field) && isset($field['name'])) {			array_push($name, $field['name']);		} else {			die('Невозможно получить поле "Имя статуса сделки"');		} 	}	} else {	die('Невозможно получить поля "Воронка продаж" и "Статусы сделки"');}if(isset($pipelines) && isset($id) && isset($name)) {	$leads_statuses = array($id, $name);	$fields = array($pipelines, $leads_statuses);	$result = json_encode($fields, JSON_UNESCAPED_UNICODE);	http_response_code(200);} else {	http_response_code(404);}?>
+<?php
+
+/**
+ * Тестовое задание.
+ * Используя данные аккаунты amoCRM и документацию для разработчиков написать небольшой скрипт,
+ * который парсит выдачу метода GET accounts/current и возвращает результат: название воронки продаж, 
+ * название статуса сделки, ID статуса сделки в формате JSON и кодом ответа сервера 200 в случае успеха. 
+ * Код скрипта предоставить. Использование сторонних классов для api amocrm не разрешается.
+ */
+ 
+$pipelines = array();
+$id = array();
+$name = array();
+ 
+if(isset($account['pipelines']) && isset($account['leads_statuses'])) {
+
+	foreach($account['pipelines'] as $field) {
+		if(is_array($field) && isset($field['name'])) {
+			array_push($pipelines, $field['name']);
+		} else {
+			die('Невозможно получить поле "Имя воронки продаж"');
+		} 
+	}
+	
+	foreach($account['leads_statuses'] as $field) {
+		if(is_array($field) && isset($field['id'])) {
+			array_push($id, $field['id']);
+		} else {
+			die('Невозможно получить поле "ID статуса сделки"');
+		} 
+	}
+	
+	foreach($account['leads_statuses'] as $field) {
+		if(is_array($field) && isset($field['name'])) {
+			array_push($name, $field['name']);
+		} else {
+			die('Невозможно получить поле "Имя статуса сделки"');
+		} 
+	}
+	
+} else {
+	die('Невозможно получить поля "Воронка продаж" и "Статусы сделки"');
+}
+
+if(isset($pipelines) && isset($id) && isset($name)) {
+	$leads_statuses = array($id, $name);
+	$fields = array($pipelines, $leads_statuses);
+	$result = json_encode($fields, JSON_UNESCAPED_UNICODE);
+	http_response_code(200);
+} else {
+	http_response_code(404);
+}
+?>
